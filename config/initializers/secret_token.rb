7e651315402b3b4cfde5b4048686c319bf3e8acd
Puneet Goyal <pungoyal@gmail.com>
2014-02-19 17:16:20 +0530

    blank app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BlankApp::Application.config.secret_key_base = '00c43ff188dc90da1eb565baad8bf0b5df23533a9b024c76a120e17a12fdd728546c99016f402a927c3f88c7fedae8cb9457db78c36131b9b4884018a210e6f0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BlankApp::Application.config.session_store :cookie_store, key: '_blank_app_session'
